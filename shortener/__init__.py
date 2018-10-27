9091b2b537cb3a3c198f6a00be3223858ae1dcab
Akshay Kumar Pall <akshay.pall@oath.com>
2018-10-26 23:46:16 -0700

    created app factory

--- shortener/__init__.py
+++ shortener/__init__.py
@@ -1,6 +1,31 @@
-from flask import Flask
-app = Flask(__name__)
+# author: Akshay Pall
 
-@app.route('/')
-def hello_world():
-	return 'Hello World!'
+import os
+from flask import Flask
+
+# Return app instance
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'shortener.sqlite')
+    )
+
+    if test_config is None:
+        # load prod config
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    # ensure instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # simple hello page for verification
+    @app.route('/running')
+    def isRunning():
+        return 'Running'
+
+    return app
